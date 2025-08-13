21069074e4da026f65b0bf561350101bf1636910
Felipe Menezes <168611944+Fehlipe19@users.noreply.github.com>
2025-08-12 21:17:27 -0400

    Add Deleteand Edit functionalities

--- index.js
+++ index.js
@@ -7,9 +7,44 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { posts: posts });
 });
 
-app.post("/submit", (req, res) => {});
+app.get("/edit", (req, res) => {
+  // const postId = parseInt(req.query.postId);
+  const postId = req.query.postId;
+  const post = posts.find((p) => p.id === postId);
+  console.log(typeof postId);
+  if (post) {
+    res.render("edit.ejs", { post: post });
+  } else {
+    res.status(404).send("Post not found");
+  }
+});
+
+app.post("/submit", (req, res) => {
+  const post = {
+    title: req.body.title,
+    content: req.body.content,
+    id: req.body.title,
+  };
+  let index = posts.findIndex((p) => p.id === post.id);
+  console.log(typeof post.id);
+  if (index !== -1 && post.id) {
+    posts[index] = post;
+  } else {
+    posts.push(post);
+  }
+  res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+  const postId = req.body.postId;
+  posts = posts.filter((post) => post.id !== postId);
+  res.redirect("/");
+});
 
 app.listen(port, () => {});
+
+//Gat info from form, add it to an object, and add it into an array.
+var posts = [];
